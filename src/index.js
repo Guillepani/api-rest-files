26ea235e5f0ac9bedc9e0b99d4d09f7224b3dba2
Guille <guillempaniagua@gmail.com>
2026-05-10 21:51:21 +0000

    chore(api): inicializar estructura base del proyecto

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API funcionando correctamente')
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
